09265ded2a51f14c4db560bf18c62773effc67f4
Robert Hardy <rhardy1@dcskk12.org>
2024-08-08 15:34:28 -0600

    Initial commit

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+//@ts-check
+/** @type {HTMLCanvasElement} */
+//@ts-ignore canvas is an HTMLCanvasElement
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 600;
