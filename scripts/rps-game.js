0fbf63ab2b96f518f370b33a45a4274f13cd9052
Robert Hardy <rhardy@dcsdk12.org>
2024-09-10 15:28:21 -0600

    Update RPS game to generate and compare weapons

--- scripts/rps-game.js
+++ scripts/rps-game.js
@@ -1,22 +1,61 @@
 //@ts-check
 
 let playerControls = document.getElementById("player-controls");
-let btnRock = document.getElementById("btn-rock");
-let btnPaper = document.getElementById("btn-paper");
-let btnScissors = document.getElementById("btn-scissors");
 
-function rockClicked(name) {
-	console.log("rock was selected");
+let isGameOver = false;
+
+let weapons = [
+	{
+		type: "Rock",
+		beats: "Scissors",
+	},
+	{
+		type: "Paper",
+		beats: "Rock",
+	},
+	{
+		type: "Scissors",
+		beats: "Paper",
+	},
+];
+
+function pickRandomWeapon(weapons) {
+	let randy = Math.floor(Math.random() * weapons.length);
+	return weapons[randy];
 }
 
-function paperClicked(name) {
-	console.log("paper was selected");
+function determineOutcome(playerWeapon, computerWeapon) {
+	if (playerWeapon.type === computerWeapon.type) {
+		return "It's a tie! Try again!";
+	}
+
+	isGameOver = true;
+
+	if (playerWeapon.beats === computerWeapon.type) {
+		return `Player wins! ${playerWeapon.type} beats ${computerWeapon.type}`;
+	}
+
+	return `Computer wins! ${computerWeapon.type} beats ${playerWeapon.type}`;
 }
 
-function scissorsClicked(name) {
-	console.log("scissors was selected");
+function playerControlHandler(e) {
+	// if (isGameOver) {
+	// 	return;
+	// }
+
+	let weaponName = e.target.innerText;
+	let playerWeapon = weapons.find((w) => w.type === weaponName);
+
+	if (!playerWeapon) {
+		console.log("ERROR! Player weapon undefined");
+		return;
+	}
+
+	let computerWeapon = pickRandomWeapon(weapons);
+
+	let result = determineOutcome(playerWeapon, computerWeapon);
+
+	console.log(result);
 }
 
-btnRock?.addEventListener("click", rockClicked);
-btnPaper?.addEventListener("click", paperClicked);
-btnScissors?.addEventListener("click", scissorsClicked);
+playerControls?.addEventListener("click", playerControlHandler);
