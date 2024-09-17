f518aca7b9120d428aee1be151795f0d2848725b
Robert Hardy <rhardy@dcsdk12.org>
2024-09-17 16:25:42 -0600

    Creating files for learning about the canvas

--- scripts/canvas-basics/app.js
+++ scripts/canvas-basics/app.js
@@ -2,6 +2,7 @@
 /** @type {HTMLCanvasElement} */
 //@ts-ignore canvas is an HTMLCanvasElement
 const canvas = document.getElementById("game-canvas");
+
+/** @type {CanvasRenderingContext2D} */
+//@ts-ignore ctx is an CanvasRenderingContext2D
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 600;
