6241095e81c3964cc29d818feb4585797a616876
Robert Hardy <rhardy@dcsdk12.org>
2024-09-18 16:20:55 -0600

    Drawing shapes

--- scripts/canvas-basics/app.js
+++ scripts/canvas-basics/app.js
@@ -6,3 +6,9 @@ const canvas = document.getElementById("game-canvas");
 /** @type {CanvasRenderingContext2D} */
 //@ts-ignore ctx is an CanvasRenderingContext2D
 const ctx = canvas.getContext("2d");
+
+ctx.fillRect(0, 0, 50, 50);
+
+ctx.beginPath();
+ctx.arc(100, 100, 25, 0, Math.PI * 2);
+ctx.fill();
