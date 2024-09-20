393d4e6cc61356efe7c1d5674b097b697eb4ae34
Robert Hardy <rhardy@dcsdk12.org>
2024-09-20 10:46:47 -0600

    Add color animation

--- scripts/canvas-basics/app.js
+++ scripts/canvas-basics/app.js
@@ -7,8 +7,29 @@ const canvas = document.getElementById("game-canvas");
 //@ts-ignore ctx is an CanvasRenderingContext2D
 const ctx = canvas.getContext("2d");
 
+ctx.fillStyle = "hsla(0, 100%, 50%, 100%)";
+//"#ff0000";
+//"red";
+
 ctx.fillRect(0, 0, 50, 50);
 
 ctx.beginPath();
 ctx.arc(100, 100, 25, 0, Math.PI * 2);
 ctx.fill();
+
+let lastTime = 0;
+let hue = 0;
+
+function drawLoop(timestamp) {
+	let elapsedTime = timestamp - lastTime;
+	lastTime = timestamp;
+
+	ctx.fillStyle = `hsla(${hue}, 100%, 50%, 100%)`;
+	ctx.fillRect(200, 200, 100, 100);
+	hue = hue + 1;
+	//	console.log(elapsedTime);
+
+	window.requestAnimationFrame(drawLoop);
+}
+
+window.requestAnimationFrame(drawLoop);
