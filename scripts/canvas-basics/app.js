b26ede257d7d151b06e21df55787ebb3dcdb3067
Robert Hardy <rhardy@dcsdk12.org>
2024-09-25 20:36:54 -0600

    Added collision detection and for loops

--- scripts/canvas-basics/app.js
+++ scripts/canvas-basics/app.js
@@ -19,12 +19,34 @@ class SquareShape {
 		this.speedMult = 11;
 		this.speedX = Math.floor(Math.random() * this.speedMult) + 1;
 		this.speedY = Math.floor(Math.random() * this.speedMult) + 1;
+		this.dirX = 1;
+		this.dirY = 1;
 	}
 
 	update() {
-		this.x += this.speedX;
-		this.y += this.speedY;
+		this.x += this.speedX * this.dirX;
+		this.y += this.speedY * this.dirY;
 		this.hue++;
+
+		if (this.hue > 360) {
+			this.hue = 0;
+		}
+
+		if (this.x < 0) {
+			// offscreen left so move right
+			this.dirX = 1;
+		} else if (this.x + this.width > canvas.width) {
+			// offscreen right so move left
+			this.dirX = -1;
+		}
+
+		if (this.y < 0) {
+			// offscreen top so move down
+			this.dirY = 1;
+		} else if (this.y + this.height > canvas.height) {
+			// offscreen bottom so move up
+			this.dirY = -1;
+		}
 	}
 
 	draw() {
@@ -34,6 +56,12 @@ class SquareShape {
 }
 
 let s1 = new SquareShape(0, 0);
+
+let shapes = [];
+
+for (let i = 0; i < 100; i++) {
+	shapes.push(new SquareShape(0, 0));
+}
 
 let lastTime = 0;
 
@@ -45,8 +73,12 @@ function drawLoop(timestamp) {
 	s1.draw();
 	s1.update();
 
+	for (const shape of shapes) {
+		shape.update();
+		shape.draw();
+	}
+
 	window.requestAnimationFrame(drawLoop);
 }
 
 window.requestAnimationFrame(drawLoop);
- 
